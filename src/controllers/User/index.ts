964a1b6187032e42d8b54fc8c3498ce74e27cf95
Leonardo Gomes <leonardogomespadilha@gmail.com>
2022-09-23 12:09:12 -0300

    removing duplicated controller

--- src/controllers/User/index.ts
+++
@@ -1,8 +0,0 @@
-import { User } from "@models/entity/User"
-import { getRepository } from "typeorm"
-
-export const getUser = async (request, response) => {
-  const userRepository = getRepository(User)
-  const user = await userRepository.find()
-  return response.json(user)
-}
