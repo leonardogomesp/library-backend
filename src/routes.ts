c9a9559970464c313104e17c90af5fbcf4809eb0
Leonardo Gomes <leonardogomespadilha@gmail.com>
2022-11-29 17:02:45 -0300

    feat(src): implementa diversas funcionalidades

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,16 @@
-import { createUser, getAllUsers, getOneUser } from "./controller/user"
+import { createBook, deleteBook, getAllBooks, getOneBook, updateBook } from "./controller/book"
+import { createUser, deleteUser, getAllUsers, getOneUser, updateUser } from "./controller/user"
 
 export const defineRoutes = (app) => {
   app.post("/user", createUser)
   app.get("/user", getAllUsers)
   app.get("/user/:id", getOneUser)
+  app.put("/user/:id", updateUser)
+  app.delete("/user/:id", deleteUser)
+  
+  app.post("/book", createBook)
+  app.get("/book", getAllBooks)
+  app.get("/book/:id", getOneBook)
+  app.put("/book/:id", updateBook)
+  app.delete("/book/:id", deleteBook)
 }
